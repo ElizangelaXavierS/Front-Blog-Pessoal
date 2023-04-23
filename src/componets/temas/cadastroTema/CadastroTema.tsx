33b60ec396616592113ce5cae062b2bca33d7e2a
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-04-23 11:04:41 -0300

    Criando os métodos Post e Put do tema.

--- src/componets/temas/cadastroTema/CadastroTema.tsx
+++ src/componets/temas/cadastroTema/CadastroTema.tsx
@@ -1,11 +1,79 @@
 import { Container, Typography, TextField, Button } from "@material-ui/core"
 import './CadastroTema.css'
+import { useNavigate, useParams } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
+import { ChangeEvent, useEffect, useState } from "react";
+import Tema from "../../../models/Tema";
+import { buscarId, post, put } from "../../../services/Service";
 
 
 function CadastroTema() {
+    const history = useNavigate();
+    const {id} = useParams<{id:string}>();
+    const [token, setToken]= useLocalStorage('token');
+    const [tema, setTema] = useState<Tema>({
+        id: 0,
+        descricao:''
+    })
+
+    useEffect(() => {
+        if(token === ""){
+            alert("Você precisar está logado!");
+            history("/login");
+        }
+    }, [token]);
+
+    useEffect(() => {
+        if(id !== undefined){
+            findById(id);
+        }
+    }, [id]);
+
+    async function findById(id: string){
+        buscarId(`/tema${id}`, setTema, {
+            headers: {
+                "Authorization" : token
+            }
+        })
+    }
+
+    function updatedTema(event: ChangeEvent<HTMLInputElement>){
+        setTema({
+            ...tema,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    async function onSubmit(event: ChangeEvent<HTMLFormElement>) {
+        event.preventDefault()
+        console.log("tema " + JSON.stringify(tema))
+
+        if (id !== undefined) {
+            console.log(tema)
+            put(`/tema`, tema, setTema, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            alert('Tema atualizado com sucesso');
+        } else {
+            post(`/tema`, tema, setTema, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            alert('Tema cadastrado com sucesso');
+        }
+        voltaTemas()
+    }
+
+    function voltaTemas() {
+        history('/temas')
+    }
+    
     return (
         <Container maxWidth="sm" className="topo">
-            <form>
+            <form onSubmit={onSubmit}>
                 <Typography 
                 variant="h3" 
                 color="textSecondary" 
@@ -16,6 +84,8 @@ function CadastroTema() {
                 </Typography>
 
                 <TextField 
+                value={tema.descricao}
+                onChange={(event: ChangeEvent<HTMLInputElement>) => updatedTema(event)} 
                 id="descricao" 
                 label="Descrição" 
                 variant="outlined" 
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -19,5 +19,22 @@ export const buscar = async(url: any, setDados: any, header: any) => {
     setDados(resposta.data)
 }
 
+export const buscarId = async(url: any, setDados: any, header: any) => {
+    const resposta = await api.get(url,header)
+    setDados(resposta.data)
+}
+
+export const post = async(url: any, dados: any, setDados: any, header: any) => {
+    const resposta = await api.post(url,dados, header)
+    setDados(resposta.data)
+}
+
+export const put = async(url: any, dados: any, setDados: any, header: any) => {
+    const resposta = await api.put(url,dados, header)
+    setDados(resposta.data)
+}
+export const deleteId = async(url: any, header: any) => {
+    await api.delete(url, header)
+}
 
 
